1df19c71b05103ed392f5a0be715c7fa73e05d62
Reymond Ramirez <re410916@ucf.edu>
2025-03-08 00:53:20 -0500

    Added main and test files and program

---
+++ test/test_assignment_3.py
@@ -0,0 +1,38 @@
+def f(t, y):
+    return t - y**2
+
+def euler_method(f, x0, y0, t_end, iter):
+    h = (t_end - x0) / iter  
+    t = x0
+    y = y0
+    for _ in range(iter):
+        y = y + h * f(t, y)
+        t = t + h
+    return y
+
+def rk(f, x0, y0, t_end, iter):
+    h = (t_end - x0) / iter  
+    t = x0
+    y = y0
+    for _ in range(iter):
+        k1 = f(t, y)
+        k2 = f(t + h/2, y + h/2 * k1)
+        k3 = f(t + h/2, y + h/2 * k2)
+        k4 = f(t + h, y + h * k3)
+        y = y + (h/6) * (k1 + 2*k2 + 2*k3 + k4)
+        t = t + h
+    return y
+
+def main():
+    
+    x = 0
+    y = 1
+    end = 2
+    iterations = 10
+
+    euler_result = euler_method(f, x, y, end, iterations)
+    rk_result = rk(f, x, y, end, iterations)
+
+    print( euler_result)
+    print(rk_result)
+main()
